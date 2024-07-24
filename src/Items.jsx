dd27a8d1fe5e365b9baae9b2c573014f9f4868b6
Sholola-Gbolahan <shololagbolahan@gmail.com>
2024-07-25 00:21:48 +0100

    Single Items list Get query process

--- src/Items.jsx
+++ src/Items.jsx
@@ -1,11 +1,25 @@
-import SingleItem from './SingleItem';
+//1. import useQuery from react-query
+import { useQuery } from "@tanstack/react-query"
+import SingleItem from "./SingleItem"
+import customFetch from "./utils"
+
 const Items = ({ items }) => {
+  // 2. setup an instance to store return data
+  //3. useQuery need queryKey and queryFn
+  const result = useQuery({
+    // 4.  The unique key you provide is used internally for refetching, caching, and sharing your queries throughout your application.
+    queryKey: ["task"],
+    //5. A query function can be literally any function that returns a promise. The promise that is returned should either resolve the data or throw an error.
+    queryFn: () => customFetch.get("/"),
+  })
+
+  console.log(result)
   return (
-    <div className='items'>
+    <div className="items">
       {items.map((item) => {
-        return <SingleItem key={item.id} item={item} />;
+        return <SingleItem key={item.id} item={item} />
       })}
     </div>
-  );
-};
-export default Items;
+  )
+}
+export default Items
--- src/main.jsx
+++ src/main.jsx
@@ -3,16 +3,13 @@ import ReactDOM from "react-dom/client"
 import App from "./App"
 import "./index.css"
 import "react-toastify/dist/ReactToastify.css"
-//1.Import queryClient and queryClientProvider from react-query
+
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query"
 
-//3. create an instance to store the queryClient
-const queryClient = queryClient()
+const queryClient = new QueryClient()
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  // 2. Wrap our entire application inside of the queryClient provider
-  //4. pass the queryClient down using  props of client
-  <QueryClientProvider client= {queryClient}>
+  <QueryClientProvider client={queryClient}>
     <App />
   </QueryClientProvider>
 )
