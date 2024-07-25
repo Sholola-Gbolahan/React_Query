ac7db054ddebdeef98ef072a55810fc57841677b
Sholola-Gbolahan <shololagbolahan@gmail.com>
2024-07-25 23:56:49 +0100

    Delete Task Functionality done

--- src/SingleItem.jsx
+++ src/SingleItem.jsx
@@ -13,6 +13,16 @@ const SingleItem = ({ item }) => {
     },
   })
 
+  const { mutate: deleteTask } = useMutation({
+    // Passing the required Id to query
+    mutationFn: () => customFetch.delete(`/${item.id}`),
+    // 1. Getting task update on edit success
+    onSuccess: () => {
+      // This generated updated list immediately on change success
+      queryClient.invalidateQueries({ queryKey: "task" })
+    },
+  })
+
   return (
     <div className="single-item">
       <input
@@ -33,7 +43,7 @@ const SingleItem = ({ item }) => {
       <button
         className="btn remove-btn"
         type="button"
-        onClick={() => console.log("delete task")}
+        onClick={() => deleteTask()}
       >
         delete
       </button>
